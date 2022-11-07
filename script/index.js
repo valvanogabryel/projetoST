2899cecbacfd951425d0b5f8bc92262fd3a822ff
valvanogabryel <valvanogabryel@gmail.com>
2022-11-07 16:47:55 -0300

    audio-fix

--- script/index.js
+++ script/index.js
@@ -1,5 +1,12 @@
 function switchTheme() {
     document.body.classList.toggle('dark-theme');
     document.body.classList.toggle('light-theme');
+    const theme = document.body.classList[0];
+    const music = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg';
+
+    const audio = document.querySelector('audio#music');
+    audio.src = `assets/musics/${music}`;
+    audio.play()
+    audio.volume = 0.2;
 }
 
