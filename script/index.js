e267fc5af4aeaffb6ffe9618fcb35fce149f7778
valvanogabryel <valvanogabryel@gmail.com>
2022-11-10 14:00:16 -0300

    scroll_reveal

--- script/index.js
+++ script/index.js
@@ -1,9 +1,26 @@
 document.getElementById('music').volume = 0.2
 window.sr = ScrollReveal({ reset: true })
-sr.reveal('.reveal-1', { duration: 1000 })
-sr.reveal('.reveal-2', { duration: 1000 })
-sr.reveal('.reveal-3', { duration: 1000 })
-sr.reveal('.reveal-4', { duration: 1000 })
+sr.reveal('.reveal-1', {
+    rotate: { x: 80, y: 0, z: 0 },
+    duration: 1500
+})
+sr.reveal('.reveal-2', {
+    rotate: { x: 0, y: 80, z: 0 },
+    duration: 1500
+})
+sr.reveal('.reveal-3', {
+    rotate: { x: 80, y: 0, z: 0 },
+    duration: 1500
+})
+sr.reveal('.reveal-4', {
+    rotate: { x: 80, y: 0, z: 0 },
+    duration: 1500
+})
+
+sr.reveal('.rotate-reveal', {
+    rotate: { x: 0, y: 0, z: 80 },
+    duration: 1500
+})
 
 
 function switchTheme() {
